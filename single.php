575489e30b3dfe3b8024055edbff985da7934ee4
Martin <emartin@cmaisonneuve.qc.ca>
2025-02-25 13:06:09 -0500

    amélioration du ffoter ajout de wp_nav_menu()  pour les lien externe

--- single.php
+++ single.php
@@ -5,12 +5,14 @@
  */
 ?>
 <?php get_header() ?>
-<h1>index.php</h1>
+<h1>single.php</h1>
     <section class="populaire">
         <div class="global">
             <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
             <article class="populaire__article">
-             
+                <?php 
+                if (has_post_thumbnail())
+                the_post_thumbnail(); ?>
                 <h2 class="populaire__titre"><?php the_title(); ?></h2>
                 <div class="populaire__contenu"><?php the_content(); ?></div>
             </article>
