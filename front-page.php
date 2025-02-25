e35fa1c927f74a7b970dc558b7bc95e08d569d35
Martin <emartin@cmaisonneuve.qc.ca>
2025-02-25 10:06:19 -0500

    s6c1 utilisation des iimages mise en avant: the_post_thumbnail()

--- front-page.php
+++ front-page.php
@@ -47,9 +47,14 @@
                     <img src="images/img1.jpg" alt="Image de voyage">
                 </figure>
                 <div class="carte__contenu">
+                    <?php
+                    if (has_post_thumbnail()) {
+                        // permet d'afficher la petite image associé à l'article (image mise en avant)
+                        the_post_thumbnail('thumbnail'); }
+                    ?>
                     <h2 class="carte__titre"><?php the_title(); ?></h2>
                     <p class="carte__description"><?php echo wp_trim_words(get_the_excerpt(), 20, "...") ; ?></p>
-                    <button class="carte__bouton carte__bouton--actif">Suite</button>
+                    <a class="carte__bouton carte__bouton--actif"  href="<?php the_permalink(); ?>">Suite</a>
                 </div>
             </article>
             <?php } ?>
