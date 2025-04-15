7a9ec823c89d1a77347ac65f11aa08e3e74d1be3
Martin <emartin@cmaisonneuve.qc.ca>
2025-04-15 09:25:17 -0400

    s11 début du carrousel

--- js/destination.js
+++ js/destination.js
@@ -2,35 +2,44 @@
     console.log("vive Javascript")
     
         let categoryId = 3; // Remplacez par l'ID de la catégorie souhaitée
+        //const domaine = "http://localhost/4w4-gr1"
         const domaine = window.location.href
         //const apiUrl = `${domaine}wp-json/wp/v2/posts?categories=${categoryId}`;
         let apiUrl =  `${domaine}wp-json/wp/v2/posts?categories=${categoryId}`;
         const categorie__ul__li = document.querySelectorAll(".categorie__ul__li")
         console.log("categorie__ul__li.length", categorie__ul__li.length)
+        mon_fetch(apiUrl)
         categorie__ul__li.forEach(li => {
-            li.addEventListener("click",function(){
+            li.addEventListener("mousedown",function(){
                console.log(li.dataset.id) 
                categoryId = li.dataset.id
                apiUrl = `${domaine}wp-json/wp/v2/posts?categories=${categoryId}`;
                console.log("apiUrl = ", apiUrl)
+               mon_fetch(apiUrl)
             })
             
         })
+
+        function mon_fetch(apiUrl)
+        {
         fetch(apiUrl)
             .then(response => response.json())
             .then(data => {
                 //  <div>${article.excerpt.rendered}</div>
                 const destinationList = document.querySelector('.destination__list');
+                destinationList.innerHTML = ""
                 data.forEach(article => {
                     const articleElement = document.createElement('div');
                     articleElement.innerHTML = `
                         <h3>${article.title.rendered}</h3>
-                       
+                        <p>${article.excerpt.rendered}</p>
+
                         <a href="${article.link}">Lire plus</a>
                     `;
                     destinationList .appendChild(articleElement);
                 });
             })
             .catch(error => console.error('Erreur lors de la récupération des articles:', error));
+        }
 
 })()
