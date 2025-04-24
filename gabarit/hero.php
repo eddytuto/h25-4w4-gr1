3177ee720f35328cd6e3778c22a6ed9e082f056f
Martin <emartin@cmaisonneuve.qc.ca>
2025-04-24 10:15:35 -0400

    s12c2 introduction aux image svg sépateur de footer

--- gabarit/hero.php
+++ gabarit/hero.php
@@ -10,7 +10,11 @@ $hero_background[$k] = get_theme_mod('hero_background_' . $k, 'Default Title');
     <div class="hero__carrousel"  style="background-image: url('<?php echo $hero_background[0] ?>');" ></div>    
     <div class="hero__carrousel"  style="background-image: url('<?php echo $hero_background[1] ?>');" ></div> 
     <div class="hero__carrousel"  style="background-image: url('<?php echo $hero_background[2] ?>');" ></div> 
-    
+    <div class="hero__radio">
+        <input class="hero__radio__input" type="radio" name="carrousel" data-id_carrousel="0"  checked="checked">
+        <input class="hero__radio__input" type="radio" name="carrousel" data-id_carrousel="1">
+        <input class="hero__radio__input" type="radio" name="carrousel" data-id_carrousel="2">
+    </div>
     <!-- ///////////////////////////////////////////////// hero__contenu -->
     <div class="hero__contenu global">
         <h1 class="hero__titre">
--- js/carrousel.js
+++ js/carrousel.js
@@ -1,5 +1,22 @@
 (function(){
-console.log ("carrousel.js")
+    console.log ("carrousel.js")
+    let hero__radio__input = document.querySelectorAll(".hero__radio__input")
+    let hero__carrousel = document.querySelectorAll(".hero__carrousel")
+    console.log("hero__radio__input.length = ", hero__radio__input.length)
 
+    hero__radio__input.forEach(elm=> {
+        console.log("elm.id_carrousel = ", elm.dataset.id_carrousel)
+        elm.addEventListener('mousedown',function(){
+            console.log(elm.dataset.id_carrousel)
+            elm.classList.add('hero__carrousel--active')
+        })
+    })
+
+
+function parcourir_carrousel(){
+    hero__carrousel.forEach(img=> {
+        img.classList.remove('hero__carrousel--active')
+    })
+}
 
 })()
